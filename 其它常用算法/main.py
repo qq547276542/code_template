ce89b20288f70f6adb8fd59b161e8e5553c4b668
qq547276542 <547276542@qq.com>
2019-04-11 21:33:27 +0800

    update raw_input ---> input

--- 其它常用算法/main.py
+++ 其它常用算法/main.py
@@ -12,40 +12,40 @@ print后面加逗号就不会换行，否则反之，当然3.x君自行传参
  
 while True:
 	try:
-		a, b = map(int, raw_input().strip().split())
+		a, b = map(int, input().strip().split())
 		print(a + b)
 	except EOFError:
 		break
 	
 # 输入一个整数，告诉我们接下来有多少组数据，然后在输入每组数据的具体值。
  
-tcase = int(raw_input().strip())
+tcase = int(input().strip())
 for case in range(tcase):
-	a, b = map(int, raw_input().strip().split())
+	a, b = map(int, input().strip().split())
 	print(a + b)
 	
 # 有多组输入数据，没有具体的告诉你有多少组,但是题目却告诉你遇见什么结束
  
 while True:
-	a, b = map(int, raw_input().strip().split())
+	a, b = map(int, input().strip().split())
 	if a == 0 and b == 0:
 		break
 	print(a + b)
 	
 # 输入有多组，并却题目告诉你每组输入遇见什么结束，与第三种不同之处在于，每组输入都有相应的细化。
 	
-tcase = int(raw_input().strip())
+tcase = int(input().strip())
 for case in range(tcase):
-	a, b = map(int, raw_input().strip().split())
+	a, b = map(int, input().strip().split())
 	if a == 0 and b == 0:
 		break
 	print(a + b)
 	
 # 这次的输入实现输入一个整数，告诉我们有多少行，在输入每一行。对于每一行的输入，有划分为第一个数和其他的数，第一个数代表那一组数据一共有多少输入。
  
-tcase = int(raw_input().strip())
+tcase = int(input().strip())
 for case in range(tcase):
-	data = map(int, raw_input().strip().split())
+	data = map(int, input().strip().split())
 	n, array = data[0], data[1:]
 	
 	sum = 0
@@ -57,7 +57,7 @@ for case in range(tcase):
  
 while True:
 	try:
-		data = map(int, raw_input().strip().split())
+		data = map(int, input().strip().split())
 		n, array = data[0], data[1:]
 			
 		sum = 0
@@ -71,7 +71,7 @@ while True:
  
 while True:
 	try:
-		a, b = map(int, raw_input().strip().split())
+		a, b = map(int, input().strip().split())
 		print(a + b)
 	except EOFError:
 		break
@@ -79,7 +79,7 @@ while True:
 # 这种类型的输出注意的就是换行，这类题目说在输出样例中，每组样例之间有什么什么，所以我们在对应输出的同时要判断一下是否是最后一组输出，如果不是，就 将题目所说的东西输出（一般是换行或空格），如果是，就直接结束。
  
 while True:
-	data = raw_input().strip()
+	data = input().strip()
 	if data.isspace():
 		break
 	else:
